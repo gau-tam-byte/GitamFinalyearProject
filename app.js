6b80146bbc7fa05939b42dd0bda17cd8e72c9fd0
gau-tam-byte <gautamkumark1214@gmail.com>
2023-03-16 13:27:18 +0530

    backendcode

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+// const morgan = require('morgan')
+const path = require('path');
+const cookieparser = require('cookie-parser');
+// const expressHandlerbars = require('express-handlebars')
+// const flash = require('connect-flash');
+const session = require('express-session')
+// const mongoose = require('mongoose');
+const passport = require('passport');
+require('../Backend/db/dbconnection')
+// mongoose.Promise= global.Promise
+// mongoose.connect('mongodb://127.0.0.1:27017/?directConnection=true')
+
+// 1 
+const app = express()
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieparser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(session({
+  cookie: { maxAge: 1000001 },
+  secret: 'connect.sid',
+  saveUninitialized: true,
+  resave: true,
+}));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
+
+
+app.use('/',require('./routes/index'))
+
+app.use('/',require('./routes/users'))
+
+
+app.listen(5000, () => console.log('Server started listening on port 5000!'))
